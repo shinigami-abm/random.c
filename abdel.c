a775ef5a422f34a89ec18491d88f134d574088ba
AMBILZ <abdelmoise1@gmail.com>
2025-07-19 20:16:05 +0100

    code c for odd and even numbers

---
+++ abdel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+
+	
+   printf("give me a value of n\n");
+   printf("n=");
+   scanf("%d",&n);
+
+   if(n%2 == 0){
+	   printf("even\n");
+   }
+   else{
+	   printf("odd\n");
+   }
+
+}
